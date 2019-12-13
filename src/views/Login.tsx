0c0923a0139301b9f41f6176b11af7d0ba2adc62
cmangelo <christianangelo112@msn.com>
2019-12-13 13:09:04 -0800

    Create basic login component

---
+++ src/views/Login.tsx
@@ -0,0 +1,29 @@
+import React, { FormEvent } from 'react';
+
+const onFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const target = event.target as any;
+    const username = target.elements.username.value.trim();
+    const password = target.elements.password.value.trim();
+    const response = await fetch('http://localhost:3000/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    });
+    const json = await response.json();
+    console.log(json);
+}
+
+export const Login: React.FC = () => {
+    return (
+        <div>
+            <form action="" onSubmit={onFormSubmit}>
+                <input type="text" placeholder="Username" name="username" />
+                <input type="password" placeholder="Password" name="password" />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
