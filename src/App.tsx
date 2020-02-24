e32824ee3117d2d068183d83d71e6f66c1b1038d
cmangelo <christianangelo112@msn.com>
2020-02-24 07:15:02 -0800

    Set up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import './App.scss';
 
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 import DraftBoard from './views/DraftBoard';
+import { Login } from './views/Login';
+import Rankings from './views/Rankings';
 
 const App: React.FC = () => {
   return (
     <div className="App container">
+      <Router>
+        <div>
+          <nav>
+            <Link to="/rankings">Rankings</Link>
+            &nbsp;
+            <Link to="/board">Board</Link>
+          </nav>
+
+          <Switch>
+            <Route path="/rankings">
+              <Rankings />
+            </Route>
+            <Route path="/board">
+              <DraftBoard />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       {/* <Login></Login> */}
       {/* <Rankings></Rankings> */}
       <DraftBoard></DraftBoard>
