ee813232d13a6d493577e9759187cc1677a47843
cmangelo <christianangelo112@msn.com>
2020-03-14 13:22:14 -0700

    Fix bug affecting ios builds

--- src/store/storeConfig.ts
+++ src/store/storeConfig.ts
@@ -1,4 +1,5 @@
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
@@ -8,8 +9,8 @@ export const endpoint = localDev ? 'http://localhost:3000/' : 'http://157.245.23
 
 export default createStore(
     rootReducer,
-    compose(
+    composeWithDevTools(
         applyMiddleware(thunk.withExtraArgument(endpoint)),
-        (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+        // (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
